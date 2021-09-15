e55860d2cd9336c7cf3fa4af6e6413016ef16788
Estefaniahurtadog12 <89108387+Estefaniahurtadog12@users.noreply.github.com>
2021-09-14 22:40:37 -0500

    coment asistente.h

--- Model/Asistente.h
+++ Model/Asistente.h
@@ -5,18 +5,20 @@
 #include <stdlib.h>
 #include <vector>
 #include "Persona.h"
-
+//incluimos la librerias que necesitamos
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
+//hereda de clase padre Personsa sus atributos 
 class Asistente: public Persona
 {
     private:
         vector<int> actas;
     public:
+    //metodos de la clase
         Asistente();
         Asistente(string nombre, int documento);
         void generarActa(int numeroTrabajo, Acta* acta);
